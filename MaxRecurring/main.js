17768dd84d50f9f4be05befece8bc0c395715650
Enaisha <Enaisha15@gmail.com>
2021-12-09 17:24:27 -0500

    cw

---
+++ MaxRecurring/main.js
@@ -0,0 +1,19 @@
+function maxRecurringChar(text) {
+  let charMap = {}
+  let charArray =[]
+  let vaulesArray = []
+  let maxCharValue = 0
+
+  for (let char of text) {
+      if (charMap.hasOwnProperty(char)) {
+          charMap[char]++
+      } else {
+          charMap[char] = 1
+      }
+  }
+  charArray = Object.keys(charMap)
+  vaulesArray = Object.values(charMap)
+  maxCharValue = Math.max(...vaulesArray)
+  
+  return charArray[vaulesArray.indexOf(maxCharValue)]
+}
