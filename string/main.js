f58d0028767f0dcae9142217180c56224c5c6dc1
Enaisha <Enaisha15@gmail.com>
2021-12-18 21:09:21 -0500

    cw

---
+++ string/main.js
@@ -0,0 +1,26 @@
+function Dad(){
+  
+  return d1+a2+d2;
+}
+function Bee(){
+  
+  return b1+e2+e2;
+}
+function banana(){
+  
+  return b2+a2+n2+a2+n2+a2;
+}
+
+
+function answer1(){
+  
+  return "no";
+}
+function answer2(){
+  
+  return "no";
+}
+function answer3(){
+ 
+  return "yes";
+}
