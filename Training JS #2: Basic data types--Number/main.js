f959e3f175905ed641adc88676bfae7c85bca0ae
Enaisha <Enaisha15@gmail.com>
2022-01-26 18:28:10 -0500

    cw

---
+++ Training JS #2: Basic data types--Number/main.js
@@ -0,0 +1,20 @@
+function equal2(){
+  var a=v3 //set number value to a
+  var b=v1 //set number value to b
+  return a-b;
+}
+function equal3(){
+  var a=v1  //set number value to a
+  var b=v5   //set number value to b
+  return a*b;
+}
+function equal4(){
+  var a=v4  //set number value to a
+  var b=v5  //set number value to b
+  return a/b;
+}
+function equal5(){
+  var a=v6   //set number value to a
+  var b=v3   //set number value to b
+  return a%b;
+}
